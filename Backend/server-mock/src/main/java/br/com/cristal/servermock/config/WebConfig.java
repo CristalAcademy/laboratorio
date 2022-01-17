74574d8fdadba216ac2a521b02aa257c560471d0
Bruno Vidal <bruno.vidal@opah.com.br>
2022-01-17 18:12:23 -0300

    add config

--- Backend/server-mock/src/main/java/br/com/cristal/servermock/config/WebConfig.java
+++ Backend/server-mock/src/main/java/br/com/cristal/servermock/config/WebConfig.java
@@ -1,2 +1,14 @@
-package br.com.cristal.servermock.config;public class WebConfig {
+package br.com.cristal.servermock.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
 }
